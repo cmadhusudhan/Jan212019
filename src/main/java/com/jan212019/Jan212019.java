21e12dc118511419d439a8f95bab6b79f501d737
mchinnala <mchinnala@SHVLSP2012F1L.SLS.ads.valuelabs.net>
2019-01-21 17:25:27 +0530

    Commited

---
+++ src/main/java/com/jan212019/Jan212019.java
@@ -0,0 +1,12 @@
+package com.jan212019;
+
+public class Jan212019 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+	System.out.println("Hi Jan 21");
+
+	}
+
+}

db6d8ce829de31d23157542709e18cc919fac788
mchinnala <mchinnala@SHVLSP2012F1L.SLS.ads.valuelabs.net>
2019-01-22 13:00:06 +0530

    Shell

--- ci/docker_entrypoint.sh
+++ ci/docker_entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-java -cp ${TRAVIS_HOME}.m2/repository/Jan212019/Jan212019/${TRAVIS_BUILD_NUMBER}/Jan212019-${TRAVIS_BUILD_NUMBER}.jar com.first.travis.example.FirstCiExample
+echo "TRAVIS_HOME : ${TRAVIS_HOME}"
+echo "TRAVIS_BUILD_NUMBER : ${TRAVIS_BUILD_NUMBER}"
+
+java -cp ${TRAVIS_HOME}/repository/Jan212019/Jan212019/${TRAVIS_BUILD_NUMBER}/Jan212019-${TRAVIS_BUILD_NUMBER}.jar com.first.travis.example.FirstCiExample

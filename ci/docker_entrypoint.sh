4dc4205d4c6024e886de3f68e0c476dcc09b66b6
mchinnala <mchinnala@SHVLSP2012F1L.SLS.ads.valuelabs.net>
2019-01-22 13:19:51 +0530

    AAAAAAAAAAAAAAAAAAAAAAA

--- ci/docker_entrypoint.sh
+++ ci/docker_entrypoint.sh
@@ -3,6 +3,8 @@
 echo "TRAVIS_HOME : ${TRAVIS_HOME}/.m2/repository/Jan212019/Jan212019/${TRAVIS_BUILD_NUMBER}/Jan212019-${TRAVIS_BUILD_NUMBER}.jar"
 echo "TRAVIS_BUILD_NUMBER : ${TRAVIS_BUILD_NUMBER}"
 
-java -cp  `basename target/*jar` com.first.travis.example.FirstCiExample
+java -cp `basename target/*jar` com.first.travis.example.FirstCiExample
 
 ## && chmod +x ci/docker_entrypoint.sh && sh ci/docker_entrypoint.sh
+
+## script: mvn --show-version clean install && java -cp /home/travis/.m2/repository/Jan212019/Jan212019/12/Jan212019-12.jar com.first.travis.example.FirstCiExample

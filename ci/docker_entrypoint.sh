2a6047ecd8d83cec97a24cc49750ea4abf24c2f0
mchinnala <mchinnala@SHVLSP2012F1L.SLS.ads.valuelabs.net>
2019-01-22 12:42:11 +0530

    Final Comit

---
+++ ci/docker_entrypoint.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+java -cp ${TRAVIS_HOME}.m2/repository/Jan212019/Jan212019/${TRAVIS_BUILD_NUMBER}/Jan212019-${TRAVIS_BUILD_NUMBER}.jar com.first.travis.example.FirstCiExample
---
+++ src/main/java/com/first/travis/example/FirstCiExample.java
@@ -0,0 +1,11 @@
+package com.first.travis.example;
+
+public class FirstCiExample {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hi Jan 21");
+
+	}
+
+}
